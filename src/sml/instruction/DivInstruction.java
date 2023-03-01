3f6b3c3146a0fa4027ea894b26b401d643c7d37e
faizaafazal <ffazal01@dcs.bbk.ac.uk>
2023-03-01 20:24:23 +0000

    Added in variables and constructor to DivInstruction

--- src/sml/instruction/DivInstruction.java
+++ src/sml/instruction/DivInstruction.java
@@ -1,6 +1,45 @@
 package sml.instruction;
 
 import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
 
 public class DivInstruction extends Instruction {
+    private final RegisterName result;
+    private final RegisterName source;
+
+    public static final String OP_CODE = "div";
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label  optional label (can be null)
+     * @param result
+     * @param source
+     */
+    public DivInstruction(String label, RegisterName result, RegisterName source) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.source = source;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Instruction i) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
