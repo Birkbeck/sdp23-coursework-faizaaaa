5f467d0c833faef8c57c56a7df9061691a5eb705
faizaafazal <ffazal01@dcs.bbk.ac.uk>
2023-03-01 22:54:50 +0000

    Implemented execute class in MovInstruction

--- src/sml/instruction/MovInstruction.java
+++ src/sml/instruction/MovInstruction.java
@@ -1,6 +1,49 @@
 package sml.instruction;
 
 import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+// MovInstruction class, to be used with Machine class. Helps interpret the sml instructions for mov.
 
 public class MovInstruction extends Instruction {
+
+    private final RegisterName result;
+    private final int input;
+
+    public static final String OP_CODE = "mov";
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label  optional label (can be null)
+     * @param result
+     * @param input
+     */
+    public MovInstruction(String label, RegisterName result, int input) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.input = input;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        m.getRegisters().set(result, input);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Instruction i) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
--- test/sml/instruction/MovInstructionTest.java
+++ test/sml/instruction/MovInstructionTest.java
@@ -1,4 +1,54 @@
 package sml.instruction;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+import static sml.Registers.Register.EBX;
+
 public class MovInstructionTest {
+
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        Instruction instruction = new MovInstruction(null, EAX, 1);
+        instruction.execute(machine);
+        Assertions.assertEquals(1, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidTwo() {
+
+    }
+
+    @Test
+    void equalsTest() {
+
+    }
+
+    @Test
+    void hashcodeTest() {
+
+    }
 }
