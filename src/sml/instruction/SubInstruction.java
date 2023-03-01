cbae9be2ce23a78b854af52905bc762fbfc81525
faizaafazal <ffazal01@dcs.bbk.ac.uk>
2023-03-01 11:01:48 +0000

    added in SubInstruction and SubInstructionTest

---
+++ src/sml/instruction/SubInstruction.java
@@ -0,0 +1,46 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+public class SubInstruction extends Instruction {
+    private final RegisterName result;
+    private final RegisterName source;
+
+    public static final String OP_CODE = "sub";
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label  optional label (can be null)
+     * @param opcode operation name
+     * @param result
+     * @param source
+     */
+    public SubInstruction(String label, String opcode, RegisterName result, RegisterName source) {
+        super(label, opcode);
+        this.result = result;
+        this.source = source;
+    }
+
+    @Override
+    public int execute(Machine machine) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Instruction i) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
