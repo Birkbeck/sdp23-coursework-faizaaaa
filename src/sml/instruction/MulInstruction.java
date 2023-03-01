671ade096e6565b9b9147905592f3d0b7fb5ba2e
faizaafazal <ffazal01@dcs.bbk.ac.uk>
2023-03-01 18:17:30 +0000

    Implemented functions in MulInstruction and added to switch statement in Translator

--- src/sml/instruction/MulInstruction.java
+++ src/sml/instruction/MulInstruction.java
@@ -4,6 +4,8 @@ import sml.Instruction;
 import sml.Machine;
 import sml.RegisterName;
 
+import java.util.Objects;
+
 // MulInstruction class, to be used with Machine class. Helps interpret the sml instructions for mul.
 
 public class MulInstruction extends Instruction {
@@ -27,22 +29,43 @@ public class MulInstruction extends Instruction {
     }
 
     @Override
-    public int execute(Machine machine) {
-        return 0;
+    public int execute(Machine m) {
+        int value1 = m.getRegisters().get(result);
+        int value2 = m.getRegisters().get(source);
+        m.getRegisters().set(result, value1 * value2);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
     }
 
     @Override
     public String toString() {
-        return null;
+        return getLabelString() + getOpcode() + " " + result + " " + source;
     }
 
     @Override
     public boolean equals(Instruction i) {
-        return false;
+        if(i.getOpcode().equals(this.opcode)) {
+            MulInstruction b = (MulInstruction) i;
+
+            if(this.label!=null&&i.getLabel()!=null)
+            {
+
+                return this.label.equals(i.getLabel())
+                        &&this.result.equals(((MulInstruction) i).result)
+                        &&this.source.equals(((MulInstruction) i).source);
+
+            }
+            else if(this.label==null&&i.getLabel()==null){
+
+                return this.result.equals(((MulInstruction) i).result)
+                        &&this.source.equals(((MulInstruction) i).source);
+            }
+            return false;
+        }
+        else {return false;}
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return Objects.hash(result,source,opcode,label);
     }
 }
